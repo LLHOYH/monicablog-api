487d8613c4072f211e8b60ef4b253dd903ed22d5
LLHOYH <llhoyh01@gmail.com>
2023-09-17 17:27:05 +0930

    fixed cors

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const corsOptions = {
     }
   },
 };
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), function () {
@@ -48,12 +48,12 @@ app.listen(app.get("port"), function () {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Content-Type", "application/json");
+  res.setHeader("Content-Type", "application/json");
   next();
 });
 
